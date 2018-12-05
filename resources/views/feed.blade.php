517b6ca1541812b89d9b05fb4de78de0945513df
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2018-12-05 11:22:54 -0200

    ajustes

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-<div class="row col-sm-12 col-md-6">
+<div class="col-sm-12 col-md-6">
 
     @foreach($posts as $post)
     <div class="col-sm-12">
@@ -59,8 +59,9 @@
 @endforeach
 </div>
 
-<div class="row col-sm-12 col-md-4">
-        @foreach($cerveja as $c)
+<div class="col-sm-12 col-md-3">
+    <h3>Recomendação</h3>
+        @foreach($cervejas as $c)
         <div class="col-sm-12">
             <div class="box box-warning">
                 <div class="box-body box-profile">
@@ -73,35 +74,29 @@
                     @endphp
                     <img class="profile-user-img img-responsive" src="{{$foto}}" alt="{{$c->nome}}">
                     <h3 class="profile-username text-center">{{$c->nome}}</h3>
-                    @if($c->fabricante_name != null)
-                    <a href="{{route('users.show', $c->fabricante->id)}}">
-                      <p class="text-muted text-center">{{$c->fabricante->fabricante_name}}</p>
-                    </a>
-                    @else
-                    <p>desconhecido</p>
-                    @endif
+                    <a href="{{route('users.show', $c->fabricante->id)}}"><p class="text-muted text-center">{{$c->fabricante->fabricante_name}}</p></a>
+                    <p>{{$c->descricao}}</p>
                     <ul class="list-group list-group-unbordered">
                         <li class="list-group-item">
                             <input id="input-1" name="input-1" class="rating rating-loading" data-min="0" data-max="5" data-step="0.1" value="{{ $c->averageRating }}" data-size="xs" disabled="">
                         </li>
-                    
+                      <li class="list-group-item">
+                         <b>Álcool por Volume</b> <a class="pull-right">{{$c->ABV}}%</a>
+                      </li>
+                      <li class="list-group-item">
+                        <b>Unidade Internacional de Amargor</b> <a class="pull-right">{{$c->IBU}}</a>
+                      </li>
                     </ul>
+                    <p>Unidades de Referência de Coloração</p>
                     <ul class="list-group list-group-unbordered">
-                      <li class="list-group-item text-center">
-                        <a type="button" href="{{ route('cervejas.show', $c->id)}}" class="btn btn-success centered">Mais Informações</a>
+                      <li class="list-group-item">
+                         <b>Método Padrão de Referência</b> <a class="pull-right">{{$c->SRM}}</a>
+                      </li>
+                      <li class="list-group-item">
+                        <b>Convenção Europeia de Cervejaria (EBC)</b> <a class="pull-right">{{$c->EBC}}</a>
                       </li>
                       <li class="list-group-item text-center">
-                        @if(Auth::user()->favoritas()->where('cerveja_id', $c->id)->first())
-          <form style="display: inline-block"method="post" action="{{route('cerveja.desfazer', $c->id)}}" onsubmit="return confirm('Quer realmente desfazer?')">   
-              {{ csrf_field() }}
-              <button type="submit"class="btn bg-teal-active centered"> favoritada </button>
-          </form>
-          @else
-            <form style="display: inline-block"method="post" action="{{route('cerveja.favoritar', $c->id)}}" onsubmit="return confirm('Deseja fazoritar esta cerveja?')">   
-                {{ csrf_field() }}
-                <button type="submit"class="btn bg-navy centered"> favoritar </button>
-            </form>
-          @endif
+                        <a type="button" href="{{ route('cervejas.show', $c->id)}}" class="btn btn-success centered">Mais Informações</a>
                       </li>
                     </ul>
                 </div>

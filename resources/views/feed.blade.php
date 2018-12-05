1722a2f90f836dc3a94c652aac4bcae0d4fb8899
Vinicius Ugoski Bacchieri <vbacch@gmail.com>
2018-12-05 11:09:22 -0200

    ajustes

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -7,56 +7,54 @@
 @stop
 
 @section('content')
-
-<div class="row col-sm-12 col-md-8">
-    @foreach($posts as $post)
-        <div class="col-sm-12">
-            <!-- Box Comment -->
-            <div class="box box-widget">
-                <div class="box-header with-border">
-                    <div class="user-block">
-                        @if($post->userimg != null)
-                        <img class="img-circle" src="{{$post->userimg}}" alt="User Image">
-                        @else
-                        <img class="img-circle" src="{{ asset('images/avatar-placeholder.svg') }}" alt="User Image">
-                        @endif
-                        <span class="username"><a href="{{ route('profile.show', $post->userId) }}">{{$post->nome}}</a></span>
-                        <span class="description">Publicado em {{$post->data}}</span>
-                    </div>
-                    <!-- /.user-block -->
-                    <div class="box-tools">
-                        <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Mark as read">
-                            <i class="fa fa-circle-o"></i></button>
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                </div>  
-                <div class="box-body">
-                        @php
-                        if (file_exists(public_path('postimages/'.$post->id.'.jpg'))) {
-                            $foto = '../postimages/'.$post->id.'.jpg';
-                            echo "<center><img class='img-responsive pad' src='$foto' alt='Photo'></center>";
-                        } else {
-                            echo "";
-                        }
-                        @endphp
-
-                        <p>{{$post->desc}}</p>
-                        @include('laravelLikeComment::like', ['like_item_id' => $post->id])
-                </div>
-
-                <div class="box-box-comments ">
-                    <a class="btn btn-primary" role="button" target="_blank" href="{{route('posts.show', $post->id)}}">visualiza comentários</a>
-                </div>
-
-                <div class="box-footer"></div>
-
+@foreach($posts as $post)
+<div class="col-md-6">
+    <!-- Box Comment -->
+    <div class="box box-widget">
+        <div class="box-header with-border">
+            <div class="user-block">
+            @if($post->userimg != null)
+                <img class="img-circle" src="{{$post->userimg}}" alt="User Image">
+             @else
+                <img class="img-circle" src="{{ asset('images/avatar-placeholder.svg') }}" alt="User Image">
+            @endif
+                <span class="username"><a href="{{ route('profile.show', $post->userId) }}">{{$post->nome}}</a></span>
+                <span class="description">Publicado em {{$post->data}}</span>
+            </div>
+            <!-- /.user-block -->
+            <div class="box-tools">
+                <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Mark as read">
+                    <i class="fa fa-circle-o"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
             </div>
         </div>
-    @endforeach
+         
+        <div class="box-body">
+            @php
+            if (file_exists(public_path('postimages/'.$post->id.'.jpg'))) {
+                $foto = '../postimages/'.$post->id.'.jpg';
+                echo "<center><img class='img-responsive pad' src='$foto' alt='Photo'></center>";
+            } else {
+                echo "";
+            }
+            @endphp
+
+            <p>{{$post->desc}}</p>
+            @include('laravelLikeComment::like', ['like_item_id' => $post->id])
+        </div>
+            
+        <div class="box-box-comments ">
+            <a class="btn btn-primary" role="button" target="_blank" href="{{route('posts.show', $post->id)}}">visualiza comentários</a>
+        </div>
+
+        <div class="box-footer"></div>
+     
+    </div>
+    <!-- /.box -->
 </div>
+@endforeach
 @stop
 @section('js')
 <script>
